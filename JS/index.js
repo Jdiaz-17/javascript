38c8cb6f8cf39008ff9e987c706f3b5c60fa785a
Juan Diaz <jadiaz2467@misena.edu.co>
2020-12-11 21:50:47 -0500

    tercer commit

--- JS/index.js
+++ JS/index.js
@@ -3,7 +3,7 @@ function ingreso (){
     var contraseña = document.getElementById("contraseña").value;
 
     if ((usuario.value == "jadiaz2467@misena.edu.co") && (contraseña.value == "juandiaz")) {
-        location.href ="www.poli.edu.co/profesional/ingenieria-de-software-virtual";
+        location.href ="https://www.poli.edu.co/profesional/ingenieria-de-software-virtual";
     }
 
     else {document.getElementById("ingreso").innerHTML = "intente nuevamente"
